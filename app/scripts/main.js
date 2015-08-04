de3071b3c9bed584120687a2258b1d5aa065d768
steve <ssaanen@gmail.com>
2015-08-04 22:03:23 +0200

    added timetable ^^

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+(function(){
+	var timetable = new Timetable();
+	timetable.setScope(18, 3);
+
+	timetable.addLocations(['Silent Disco', 'Nile', 'Len Room', 'Maas Room', 'Stevo Square', 'Congo Room', 'Dami\'s Garden']);
+	timetable.addEvent('Frankadelic', 'Stevo Square', new Date(2015,7,17,19,0), new Date(2015,7,17,20,30));
+
+	var renderer = new Timetable.Renderer(timetable);
+	renderer.draw('.timetable');
+})()
