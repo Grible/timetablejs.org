fe8f210c016d5436fc04ba8b77cb6daeb44a15ba
steve <ssaanen@gmail.com>
2015-08-11 14:55:33 +0200

    updated timetable.js to fix issues with rendering

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,8 +10,8 @@
 	timetable.addEvent('Chaka Khan', 'Nile Room', new Date(2015, 7, 17, 19, 30), new Date(2015, 7, 17, 21, 30));
 	timetable.addEvent('Laura Mvula', 'Len Room', new Date(2015, 7, 17, 22, 15), new Date(2015, 7, 17, 23, 45));
 	timetable.addEvent('Frankadelic', 'Stevo Square', new Date(2015, 7, 17, 19, 0), new Date(2015, 7, 17, 20, 30));
-	timetable.addEvent('Candi Station', 'Maas Room', new Date(2015, 7, 17, 20, 0), new Date(2015, 7, 17, 20, 40));
-	timetable.addEvent('Gregory Porter', 'Maas Room', new Date(2015, 7, 17, 20, 45), new Date(2015, 7, 17, 22, 0));
+	timetable.addEvent('Candi Station', 'Maas Room', new Date(2015, 7, 17, 20, 0), new Date(2015, 7, 17, 20, 30));
+	timetable.addEvent('Gregory Porter', 'Maas Room', new Date(2015, 7, 17, 20, 35), new Date(2015, 7, 18, 0, 30));
 	timetable.addEvent('Stevie Wonder', 'Stevo Square', new Date(2015, 7, 17, 23, 0), new Date(2015, 7, 18, 0, 45));
 	timetable.addEvent('Snarky Puppy', 'Congo Room', new Date(2015, 7, 17, 20, 30), new Date(2015, 7, 18, 2, 15));
 	timetable.addEvent('Victor Wooten', 'Dami\'s Garden', new Date(2015, 7, 17, 23, 0), new Date(2015, 7, 18, 0, 45));
